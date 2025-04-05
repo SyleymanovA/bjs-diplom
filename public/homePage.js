3a2e81932d1048b594ee42342f818e87108c7b1d
Aziz Suleymanov <9002007000@mail.ru>
2025-04-05 15:57:51 +0500

    D_Z

---
+++ public/homePage.js
@@ -0,0 +1,88 @@
+const logoutButton = new LogoutButton(); 
+logoutButton.action = () => ApiConnector.logout(() => location.reload());
+
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+
+const ratesBoard = new RatesBoard(); 
+ApiConnector.getStocks((response) => {
+  if (response.success) {
+    ratesBoard.fillTable(response.data);
+    setInterval(() => {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }, 60000);
+  }
+});
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Баланс пополнен");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Сделка прошла успешно");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Выполнен перевод валюты");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      moneyManager.setMessage(true, "Пользователь добавлен");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      moneyManager.setMessage(true, "Пользователь удален");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
